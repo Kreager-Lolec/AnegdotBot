5d97d840207470dbb0f4764b83f8655d4050be8d
unknown <juranskiy59@gmail.com>
2022-08-16 11:24:13 +0300

    change clock.py

--- clock.py
+++ clock.py
@@ -1,6 +1,3 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from app import my_interval_job
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(my_interval_job, 'cron', hour='00-24', minute='01-59')
-scheduler.start()
+my_interval_job()
