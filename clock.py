ff1f09c32804f45263ddd5aef8659e8f0005238c
unknown <juranskiy59@gmail.com>
2022-08-16 11:13:57 +0300

    change time random

--- clock.py
+++ clock.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from app import my_interval_job
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(my_interval_job, 'cron', hour='15-17', minute='01-59')
+scheduler.add_job(my_interval_job, 'cron', hour='00-24', minute='01-59')
 scheduler.start()
